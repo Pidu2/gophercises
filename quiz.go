5f29c5d1669251e9139988f4ab28979795b89b82
Beat Schärz <beatschaerz@hotmail.com>
2022-03-26 10:38:30 +0100

    done

---
+++ quiz.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+	"time"
+)
+
+func main() {
+	csvFlagPtr := flag.String("csv", "problems.csv", "csv file to use")
+	limitFlagPtr := flag.Int("limit", 30, "time for the quiz")
+	flag.Parse()
+
+	questions := readQuestions(csvFlagPtr)
+
+	timer := time.NewTimer(time.Duration(*limitFlagPtr) * time.Second)
+
+	stats := askQuestions(questions, timer)
+	fmt.Printf("%d out of %d were correct!\n", stats, len(questions))
+}
+
+func askQuestions(questions [][]string, timer *time.Timer) int {
+	correctAnswers := 0
+	for _, line := range questions {
+		var response string
+		ch := make(chan int)
+		fmt.Printf("%s = ", line[0])
+		go func() {
+			fmt.Scanln(&response)
+			ch <- 1
+		}()
+		select {
+		case <-timer.C:
+			fmt.Println("\nTIME RAN OUT!")
+			return correctAnswers
+		case <-ch:
+			if response == line[1] {
+				correctAnswers++
+			}
+		}
+	}
+	return correctAnswers
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func readQuestions(csvFlagPtr *string) [][]string {
+	f, err := os.Open(*csvFlagPtr)
+	check(err)
+	csvReader := csv.NewReader(f)
+	data, err := csvReader.ReadAll()
+	check(err)
+	return data
+}
